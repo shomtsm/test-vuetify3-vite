2c2871c0b08b963d4151b6ba7eaf3f9f0b48e204
SHO Matsushima <sho.mtsm@gmail.com>
2022-04-06 02:23:23 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-main>
+      <HelloWorld />
+
+      <div class="d-flex justify-center">
+        <v-icon>mdi-home</v-icon>
+        <v-icon>mdi-account</v-icon>
+      </div>
+      
+      <FormTest />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import FormTest from "./components/FormTest.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    HelloWorld,
+    FormTest,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
